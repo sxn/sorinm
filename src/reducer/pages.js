55e16cc672845023ba3610622b3f00751cc6d32d
Sorin Muntean <sxn@users.noreply.github.com>
2017-04-27 15:31:22 +0200

    bug: fix default `pages` state.

--- src/reducer/pages.js
+++ src/reducer/pages.js
@@ -1,10 +1,10 @@
-const bySlug = (state = {}, action) => {
+const pages = (state = [], action) => {
   switch (action.type) {
     default:
       return state;
   }
 };
 
-export default bySlug;
+export default pages;
 
 export const getPage = (state, slug) => state[slug];
