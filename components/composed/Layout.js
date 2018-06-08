647cef6ce147bf60d9d24ef45db07d96cf2f5035
Sorin Muntean <me@sorinmuntean.ro>
2018-06-08 21:24:48 +0200

    Remove analytics.

--- components/composed/Layout.js
+++ components/composed/Layout.js
@@ -2,24 +2,6 @@ import { Fragment } from "react";
 import Head from "next/head";
 import { hydrate, injectGlobal } from "emotion";
 
-const analytics = `
-  (function(i, s, o, g, r, a, m) {
-    i["GoogleAnalyticsObject"] = r;
-    (i[r] =
-      i[r] ||
-      function() {
-        (i[r].q = i[r].q || []).push(arguments);
-      }),
-      (i[r].l = 1 * new Date());
-    (a = s.createElement(o)), (m = s.getElementsByTagName(o)[0]);
-    a.async = 1;
-    a.src = g;
-    m.parentNode.insertBefore(a, m);
-  })(window, document, "script", "https://www.google-analytics.com/analytics.js", "ga");
-  ga("create", "UA-94031225-1", "auto");
-  ga("send", "pageview");
-`;
-
 const fonts = `
   @import url('https://fonts.googleapis.com/css?family=Open+Sans:400,700|Open+Sans+Condensed:300|Oswald:300,400');
 `;
@@ -63,7 +45,6 @@ export default class Layout extends React.Component {
           <link rel="manifest" href="/static/manifest.json" />
           <link rel="shortcut icon" href="/static/favicon.png" />
           <title>Sorin Muntean</title>
-          <script dangerouslySetInnerHTML={{ __html: analytics }} />
           <style dangerouslySetInnerHTML={{ __html: fonts }} />
         </Head>
         {this.props.children}
