3a2683d2713b5ed0a24fd63c44e427c36073c09d
Sorin Muntean <me@sorinmuntean.ro>
2019-09-23 08:41:43 +0200

    chore: Remove unused scripts.

--- src/store.js
+++ src/store.js
@@ -1,20 +1,8 @@
 import { applyMiddleware, createStore } from "redux";
-import { createLogger } from "redux-logger";
 import thunk from "redux-thunk";
 
 import AppState from "data";
 import reducer from "reducer";
 
-const configureStore = () => {
-  const middlewares = [thunk];
-
-  if (process.env.NODE_ENV !== "production") {
-    middlewares.push(createLogger({ duration: true, diff: true, collapsed: true }));
-  }
-
-  const store = createStore(reducer, AppState, applyMiddleware(...middlewares));
-
-  return store;
-};
-
+const configureStore = () => createStore(reducer, AppState, applyMiddleware([thunk]));
 export default configureStore;
