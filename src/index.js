12b7a5ea710bb35739c3db87b1493d13b9259ea0
Sorin Muntean <me@sorinmuntean.ro>
2020-06-10 08:31:45 +0200

    feat: enable HMR

--- src/index.js
+++ src/index.js
@@ -5,4 +5,8 @@ import "./index.css";
 import configureStore from "./store";
 import Root from "./component/composed/Root";
 
+if (module.hot) {
+  module.hot.accept();
+}
+
 render(<Root store={configureStore()} />, document.getElementById("root"));
