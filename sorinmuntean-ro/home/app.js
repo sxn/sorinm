cd7c26b472b8a6649346acd0cc06d4b7c5036a64
Sorin Muntean <me@sorinmuntean.ro>
2020-06-11 20:20:36 +0200

    rename 'pages' to 'screens'

--- sorinmuntean-ro/home/app.js
+++ sorinmuntean-ro/home/app.js
@@ -1,33 +1,33 @@
 (function () {
   window.addEventListener("load", function () {
     const state = {
-      pages: document.getElementsByClassName("Page").length,
-      currentPage: Math.round(window.scrollY / window.innerHeight),
+      pages: document.getElementsByClassName("Screen").length,
+      currentScreen: Math.round(window.scrollY / window.innerHeight),
     };
 
-    function updatePage(wantedPage, shouldScroll) {
+    function updateScreen(wantedScreen, shouldScroll) {
       // ensure the newly selected page is in the range [0, pageCount)
-      const newPage = Math.min(Math.max(wantedPage, 0), state.pages - 1);
+      const newScreen = Math.min(Math.max(wantedScreen, 0), state.pages - 1);
 
-      state.currentPage = Math.min(Math.max(newPage, 0), state.pages - 1);
+      state.currentScreen = Math.min(Math.max(newScreen, 0), state.pages - 1);
 
-      currentPageColor = document.getElementsByClassName("Page")[state.currentPage].dataset.color;
+      currentScreenColor = document.getElementsByClassName("Screen")[state.currentScreen].dataset.color;
 
       [...document.getElementsByClassName("Bullet")].forEach((bullet, index) => {
-        bullet.className = index === state.currentPage ? "Bullet active" : "Bullet";
+        bullet.className = index === state.currentScreen ? "Bullet active" : "Bullet";
 
-        if (currentPageColor) {
-          bullet.style = `background-color: ${currentPageColor}`;
+        if (currentScreenColor) {
+          bullet.style = `background-color: ${currentScreenColor}`;
         }
       });
 
       if (shouldScroll) {
-        scroll({ top: state.currentPage * window.innerHeight, behavior: "smooth" });
+        scroll({ top: state.currentScreen * window.innerHeight, behavior: "smooth" });
       }
     }
 
     function handleScroll(_e) {
-      updatePage(Math.round(window.scrollY / window.innerHeight), false);
+      updateScreen(Math.round(window.scrollY / window.innerHeight), false);
     }
 
     function disableArrows(e) {
@@ -44,7 +44,7 @@
       const kPressed = e.code === "KeyK";
 
       if (upArrowPressed || kPressed) {
-        updatePage(state.currentPage - 1, true);
+        updateScreen(state.currentScreen - 1, true);
         return;
       }
 
@@ -52,12 +52,12 @@
       const jPressed = e.code === "KeyJ";
 
       if (downArrowPressed || jPressed) {
-        updatePage(state.currentPage + 1, true);
+        updateScreen(state.currentScreen + 1, true);
         return;
       }
     }
 
-    updatePage(Math.round(window.scrollY / window.innerHeight));
+    updateScreen(Math.round(window.scrollY / window.innerHeight));
 
     document.addEventListener("scroll", handleScroll);
     document.addEventListener("keydown", disableArrows);
